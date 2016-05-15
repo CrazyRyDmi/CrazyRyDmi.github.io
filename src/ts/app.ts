227189ab32be8f40220446482fb115cd6ecfcfaf
RyDmi <rydmi@yandex.ru>
2016-05-15 23:38:14 +0400

    update

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -4,7 +4,7 @@ import * as Three from "three";
 let text = "hello world!";
 let renderer = new Three.WebGLRenderer();
 renderer.clearDepth();
-document.getElementById("main").innerText = text;
+document.getElementById("main").innerText += text;
 let a = async () => {
     console.log("doSome");
 };
