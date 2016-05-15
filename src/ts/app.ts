081343209389d678cfc8a6b501336663a723e5b7
RyDmi <rydmi@yandex.ru>
2016-05-15 20:19:24 +0400

    small changes

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,4 +3,4 @@ import * as Three from "three";
 let text = "hello world!";
 let renderer = new Three.WebGLRenderer();
 renderer.clearDepth();
-document.getElementById("main").innerText = text; 
+document.getElementById("main").innerText = text;
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,1 +1,2 @@
+/// <reference path="main/ambient/systemjs/index.d.ts" />
 /// <reference path="main/ambient/three/index.d.ts" />
