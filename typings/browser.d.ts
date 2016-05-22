8e9c0996d9fc667409df3fc0163e087b2936c1db
RyDmi <rydmi@yandex.ru>
2016-05-23 02:35:41 +0400

    update

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="browser/ambient/systemjs/index.d.ts" />
 /// <reference path="browser/ambient/three/index.d.ts" />
+/// <reference path="browser/definitions/lodash/index.d.ts" />
